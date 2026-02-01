5ae410f7e430ebb3d4b01ceea9bd1ceb1e479a5a
iouiouuoity-maker <iouiouuoity@gmail.com>
2026-02-02 02:48:20 +0500

    Create data-comics.js

---
+++ assets/js/data-comics.js
@@ -0,0 +1,5 @@
+const COMICS = [
+  {t:"QazComics", url:"https://qazcomics.kz/kz"},
+  {t:"Ер Тарғын 1", url:"https://qazcomics.kz/kz/comics/5/1"},
+  {t:"Ер Тарғын 2", url:"https://qazcomics.kz/kz/comics/5/2"}
+];
